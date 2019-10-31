887285764ee8c00379879a37f8ec4c52069fab50
NathanielLong <nlongx14@gmail.com>
2019-10-31 11:35:38 -0400

    Nathaniel initialized the Hangman

---
+++ src/hangman/MainHangman.java
@@ -0,0 +1,38 @@
+package hangman;
+import java.util.Scanner;
+public class MainHangman {
+	
+	public static void main(String[] args) {
+		Scanner scnr = new Scanner(System.in);
+		String userName;
+		String gameMode;
+		System.out.println("Dang Man, Let's Play Hang Man!");
+		System.out.println("Please enter your name: ");
+		userName = scnr.nextLine();
+		System.out.println("Please select 'Hard mode' or 'Easy mode': ");
+		gameMode = scnr.nextLine();
+		//This is where
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
