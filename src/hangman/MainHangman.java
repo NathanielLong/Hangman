51b9be3780031acff6855e659390bcd16f128d05
NathanielLong <nlongx14@gmail.com>
2019-11-01 12:34:43 -0400

    modified mainhangman

--- src/hangman/MainHangman.java
+++ src/hangman/MainHangman.java
@@ -1,4 +1,5 @@
 package hangman;
+
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -8,10 +9,13 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Scanner;
+
 public class MainHangman {
-	
-	static ArrayList<Character> guessedLetters = new ArrayList<>(); //Contains all characters that have already been guessed by the player
+
+	static ArrayList<Character> guessedLetters = new ArrayList<>(); // Contains all characters that have already been
+																	// guessed by the player
 //	static ArrayList<String> easywords = new ArrayList<>();
+
 	public static void main(String[] args) {
 		
 		Scanner scnr = new Scanner(System.in);
@@ -24,8 +28,21 @@ public class MainHangman {
 		ArrayList<String> easywords = FileHelper.readFromFile();
 		String randomStringFromEasyWords = easywords.get((int)Math.floor(Math.random() * easywords.size()));
 		System.out.println(randomStringFromEasyWords);
+		String partialWord = "";
 		for (int i = 0; i < randomStringFromEasyWords.length(); i++) {
-			System.out.print("_ ");
+			partialWord += "_";
+		}
+		for (int i = 0; (i < 10 || partialWord.equalsIgnoreCase(randomStringFromEasyWords)); i++) {
+		
+		
+		
+		char guessedSingleString = Validator.getStringMatchingRegex(scnr, "Please enter a letter: ", "[A-za-z]{1}").charAt(0);
+		guessedSingleString = Character.toUpperCase(guessedSingleString);
+		if(stringHasChar(randomStringFromEasyWords, guessedSingleString)) {
+			partialWord = (updatePartialWord(guessedSingleString, partialWord, randomStringFromEasyWords));
+		System.out.println(partialWord);
+			
+		}
 		}
 //		System.out.println("Please select 'Hard mode' or 'Easy mode': ");
 //		gameMode = scnr.nextLine();
@@ -39,12 +56,29 @@ public class MainHangman {
 		
 		
 	}
-	
-	static String replaceSingleChar(String inputString, char newChar, int newCharIndex) {
-		return inputString.substring(0,newCharIndex)+newChar+inputString.substring(newCharIndex + 1);
-	}
-	
-	
 
+	static String replaceSingleChar(String inputString, char newChar, int newCharIndex) {
+		return inputString.substring(0, newCharIndex) + newChar + inputString.substring(newCharIndex + 1);
+	}
+
+	static String updatePartialWord(char guessedChar, String revealedString, String randomString) {
+		int startIndex = 0;
+		while (true) {
+
+			startIndex = randomString.indexOf(guessedChar, startIndex);
+			if (startIndex != -1) {
+				revealedString = replaceSingleChar(revealedString, guessedChar, startIndex);
+			} else {
+				break;
+			}
+			startIndex++;
+		}
+		return revealedString;
+	}
+
+	static Boolean stringHasChar(String master, char guessedChar) {
+		return (master.indexOf(guessedChar) != -1);
+
+	}
 
 }
