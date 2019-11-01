79f9ae4406b4626850e9052e64d80795db636121
robmalec <robmalec@umich.edu>
2019-11-01 16:23:40 -0400

    Improved visuals and stuff

--- src/hangman/HangmanVisuals.java
+++ src/hangman/HangmanVisuals.java
@@ -50,15 +50,23 @@ public class HangmanVisuals {
 
 		// Clearing out console
 		clearConsole();
+		
+		for (int i = 0; i < 35; i++) {
+			System.out.print(' ');
+		}
+		System.out.println("DANG MAN!  IT'S HANGMAN!");
 
 		// Showing relevant hangman graphic
+		for (int i = 0; i <= 4; i++) {
+			System.out.println("");
+		}
 		for (String y : hangMen[guessedLetters.size()]) {
+			for (int i = 0; i < 40; i++) {
+				System.out.print(' ');
+			}
 			System.out.println(y);
 		}
 
-		// Showing word
-		printPartialWord(partialWord);
-		System.out.println("");
 
 		// Showing guessed letters
 		System.out.println("Guessed letters:");
@@ -69,14 +77,14 @@ public class HangmanVisuals {
 			System.out.println("");
 		}
 		
+		// Showing word
+		printPartialWord(partialWord);
+		System.out.println("");
 	}
 	
 	private static void printPartialWord(String partialWord) {
 		for (char c :partialWord.toCharArray()) {
-			System.out.print(c);
-			if (c == '_') {
-				System.out.print(' ');
-			}
+			System.out.print("" + c + ' ');
 		}
 	}
 
