0e907fad14c323933a8c0e905aa9c52cbaa90e95
NathanielLong <nlongx14@gmail.com>
2019-11-03 21:22:59 -0500

    made general upgrades to main hangman

--- src/hangman/MainHangman.java
+++ src/hangman/MainHangman.java
@@ -12,65 +12,68 @@ import java.util.Scanner;
 
 public class MainHangman {
 
-	static ArrayList<Character> guessedLetters = new ArrayList<>();
-
 	public static void main(String[] args) {
 
 		Scanner scnr = new Scanner(System.in);
 		StringBuilder sb = new StringBuilder();
 		String userName;
-		int missedCounter = 0;
 		int counter = 0;
 		char guessedChar = 0;
-//		String gameMode;
-//		System.out.println("Dang Man, Let's Play Hang Man!");
-		HangmanVisuals.loadHangMen();
-		HangmanVisuals.update(guessedLetters, "");
+		char userReply;
 		System.out.println("Please enter your name: ");
 		userName = scnr.nextLine();
-		String easyWord;
-		ArrayList<String> easywords = FileHelper.readFromFile();
-		String randEasyWord = easywords.get((int) Math.floor(Math.random() * easywords.size()));
-		String partialWord = "";
-		
-		for (int i = 0; i < randEasyWord.length(); i++) {
-			partialWord += "_";
-		}
-		boolean end = false;
-		while (!end) {
-			boolean done = false;
-			while (!done) {
-				guessedChar = Validator
-						.getStringMatchingRegex(scnr, userName + ", please enter a letter: ", "[A-za-z]{1}").charAt(0);
+		do {
+			ArrayList<Character> guessedLetters = new ArrayList<>();
+			int missedCounter = 0;
+			HangmanVisuals.loadHangMen();
+			HangmanVisuals.update(guessedLetters, "");
+			ArrayList<String> easywords = FileHelper.readFromFile();
+			String randEasyWord = easywords.get((int) Math.floor(Math.random() * easywords.size()));
+			String partialWord = "";
+
+			for (int i = 0; i < randEasyWord.length(); i++) {
+				partialWord += "_";
+			}
+			boolean end = false;
+			while (!end) {
+				boolean done = false;
+				while (!done) {
+					guessedChar = Validator
+							.getStringMatchingRegex(scnr, userName + ", please enter a letter: ", "[A-za-z]{1}")
+							.charAt(0);
+					guessedChar = Character.toUpperCase(guessedChar);
+					if (guessedLetters.contains(guessedChar)) {
+						System.out.println("You have already guessed this. Please enter another letter: ");
+					} else {
+						done = true;
+					}
+				}
 				guessedChar = Character.toUpperCase(guessedChar);
-				if (guessedLetters.contains(guessedChar)) {
-					System.out.println("You have already guessed this. Please enter another letter: ");
+				if (Hangman.stringHasChar(randEasyWord, guessedChar)) {
+					partialWord = (Hangman.updatePartialWord(guessedChar, partialWord, randEasyWord));
+					HangmanVisuals.update(guessedLetters, partialWord);
+					if (partialWord.equals(randEasyWord)) {
+						end = true;
+						System.out.println("Hurray, you made it!");
+						break;
+					}
+					end = false;
 				} else {
-					done = true;
+					System.out.println("Oops, this word doesn't contain this letter...");
+					guessedLetters.add(guessedChar);
+					HangmanVisuals.update(guessedLetters, partialWord);
+					missedCounter++;
+					if (missedCounter == 9) {
+						end = true;
+						System.out.println("Dang man, now we gotta eat cake!");
+					}
 				}
+
 			}
-
-			// Removed this because all it did was ask for their input again
-			// guessedChar = Validator.getStringMatchingRegex(scnr, "Please enter a letter:
-			// ", "[A-za-z]{1}").charAt(0);
-			guessedChar = Character.toUpperCase(guessedChar);
-			if (Hangman.stringHasChar(randEasyWord, guessedChar)) {
-				partialWord = (Hangman.updatePartialWord(guessedChar, partialWord, randEasyWord));
-				HangmanVisuals.update(guessedLetters, partialWord);
-				counter++;
-				end = false;
-			} else {
-				System.out.println("Oops, this word doesn't contain this letter...");
-				guessedLetters.add(guessedChar);
-				HangmanVisuals.update(guessedLetters, partialWord);
-				counter++;
-				if (missedCounter == 8) {
-					end = true;
-				}
-			}
-
-		}
-
+			System.out.println("Would you like to continue? (y/n) ");
+			userReply = scnr.nextLine().charAt(0);
+		} while (userReply == 'y');
+		System.out.println("Please come come again!");
 	}
 
 }
