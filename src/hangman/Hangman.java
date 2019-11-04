2ea83d2ffea8eac02dee79dc8d74dcf962ed9048
robmalec <robmalec@umich.edu>
2019-11-04 10:20:31 -0500

    Hangman.java

--- src/hangman/Hangman.java
+++ src/hangman/Hangman.java
@@ -3,107 +3,65 @@ package hangman;
 import java.util.Scanner;
 
 public class Hangman {
-	public static void main(String[] args) {
-
-		Scanner scan = new Scanner(System.in);
-
-		int missedLetters = 0;
-
-		String _compWord = "Hello";
-
-		System.out.println(_compWord);
-
-		// validates that the user input is a letter and one character
-		String userLetter = Validator.getStringMatchingRegex(scan, "Enter a letter: ", "[A-Za-z]{1}");
-		System.out.println(userLetter);
-
-		// replaces the computer word with underscores
-		_compWord = _compWord.replaceAll("(?i)[a-zA-z]", "_");
-		System.out.println(_compWord);
-
-		if (_compWord.contains(userLetter)) {
-			System.out.println(userLetter);
-		} else {
-			missedLetters++;
-		}
-
-	}
-
+//	public static void main(String[] args) {
+//
+//		Scanner scan = new Scanner(System.in);
+//
+//		int missedLetters = 0;
+//
+//		String _compWord = "Hello";
+//
+//		System.out.println(_compWord);
+//
+//		// validates that the user input is a letter and one character
+//		String userLetter = Validator.getStringMatchingRegex(scan, "Enter a letter: ", "[A-Za-z]{1}");
+//		System.out.println(userLetter);
+//
+//		// replaces the computer word with underscores
+//		_compWord = _compWord.replaceAll("(?i)[a-zA-z]", "_");
+//		System.out.println(_compWord);
+//
+//		if (_compWord.contains(userLetter)) {
+//			System.out.println(userLetter);
+//		} else {
+//			missedLetters++;
+//		}
+//
+//	}
+	
+	/*
+	 * Replaces a single character in inputString at location specified by newCharIndex with character
+	 * specified by newChar
+	 */
 	static String replaceSingleChar(String inputString, char newChar, int newCharIndex) {
 		return inputString.substring(0, newCharIndex) + newChar + inputString.substring(newCharIndex + 1);
 	}
 
-	static String updatePartialWord(char guessedChar, String revealedString, String randomString) {
+	/*
+	 * Replaces a character in revealedString (which is assumed to be a series of underscores) with the character guessed
+	 * by the user, in the event that this character does in fact exist in the hidden word.
+	 */
+	static String updatePartialWord(char guessedChar, String partialWord, String hiddenWord) {
 		int startIndex = 0;
 		while (true) {
 
-			startIndex = randomString.indexOf(guessedChar, startIndex);
+			startIndex = hiddenWord.indexOf(guessedChar, startIndex);
 			if (startIndex != -1) {
-				revealedString = replaceSingleChar(revealedString, guessedChar, startIndex);
+				partialWord = replaceSingleChar(partialWord, guessedChar, startIndex);
 			} else {
 				break;
 			}
 			startIndex++;
 		}
-		return revealedString;
+		return partialWord;
 	}
 
-	static Boolean stringHasChar(String master, char guessedChar) {
-		return (master.indexOf(guessedChar) != -1);
+	/*
+	 * Returns true if string word contains the character guessedChar
+	 */
+	static Boolean stringHasChar(String word, char guessedChar) {
+		return (word.indexOf(guessedChar) != -1);
 
 	}
 	
-	public String padLeftZeros(String inputString, int length) {
-	    if (inputString.length() >= length) {
-	        return inputString;
-	    }
-	    StringBuilder sb = new StringBuilder();
-	    while (sb.length() < length - inputString.length()) {
-	        sb.append('0');
-	    }
-	    sb.append(inputString);
-	 
-	    return sb.toString();
-	}
-
-//	if(_compWord.StringindexOf[userLetter]) {
-//		for(i = 0; i<_compWord.length(); i++) 
-//			System.out.println(userLetter);
-//	}
-
-//	System.out.println(isChar(userWord, computerWord));
-
-//	public static boolean isValidRegex(String userWord, String "[A-Z][a-z]{1}") {
-//		if (isValidRegex(userWord, ("[A-Z][a-z]{1}"))) {
-//		} else {
-//			System.out.println("This is not valid input. Please enter a word: ");
-//		}
-//        return userWord.matches(regex);      
-//	}
-
-//	public void checkLetter(String letter, ArrayList<> compWord) {
-//		for(int i = 0; i<compWord.length(); i++) {
-//			if(letter.equals(compWord.[i])) {
-//				// Run a method which gives user a correct letter
-//			}
-//			else {
-//				String failed = "Could not find the letter"
-//			}
-//		}
-//	}
-
-//	public static boolean isChar(String userLetter, String computerWord) {
-//	
-//		int missCounter = 0;
-//		
-//	if (computerWord.contains(userLetter)) {
-//		System.println(userLetter);
-//		return true;
-//	} else {
-//		
-//		missCounter++;
-//		return false;
-//		
-//	}
-//}
 }
