b471280693362990fc6bdecb26f28b6e778c2dd2
Nina Violet <ninacmills@gmail.com>
2019-11-04 13:13:03 -0500

    commits

--- src/hangman/MainHangman.java
+++ src/hangman/MainHangman.java
@@ -1,133 +1,3 @@
-<<<<<<< HEAD
-package hangman;
-
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.Scanner;
-
-public class MainHangman {
-
-	public static void main(String[] args) {
-		LeaderBoard.loadLeaderBoard();
-		Difficulty dLevel = Difficulty.EASY;
-		
-		Scanner scnr = new Scanner(System.in);
-		StringBuilder sb = new StringBuilder();
-		String userName;
-		int counter = 0;
-		char guessedChar = 0;
-		char userReply;
-		
-		userName = HangmanVisuals.getUserName();
-//		System.out.println("Please enter your name: ");
-//		userName = scnr.nextLine();
-		
-		if (userName.equalsIgnoreCase("Nina")) {
-			System.out.println("There is no cheating allowed and you dont have over 1000 wins...");
-			counter--;
-		}
-		if (userName.equalsIgnoreCase("Rob")) {
-			System.out.println("Bro, you just get an automatic win. You don't even have to play...");
-			counter++;
-		}
-		if (userName.equalsIgnoreCase("Nathaniel")) {
-			System.out.println("Haha, I'm gonna lose...");
-			counter = -101;
-		}
-		
-		do {
-			ArrayList<Character> guessedLetters = new ArrayList<>();
-			int missedCounter = 0;
-			HangmanVisuals.loadHangMen();
-			ArrayList<String> wordBank = FileHelper.readFromFile();
-			
-			int minLength = 0, maxLength = 0;
-			
-			switch(HangmanVisuals.getDiffLevel()) {
-			case EASY:
-				maxLength = 5;
-				break;
-			case MEDIUM:
-				minLength = 6;
-				maxLength = 9;
-				break;
-			case HARD:
-				minLength = 10;
-				maxLength = 14;
-				break;
-			}
-			
-			//String hiddenWord = wordBank.get((int) Math.floor(Math.random() * wordBank.size()));
-			String hiddenWord = Selector.getWordByLengthRange(minLength, maxLength, wordBank);
-			String partialWord = "";
-
-			for (int i = 0; i < hiddenWord.length(); i++) {
-				partialWord += "_";
-			}
-			
-			HangmanVisuals.update(guessedLetters, partialWord);
-			boolean end = false;
-			while (!end) {
-				boolean done = false;
-				while (!done) {
-					guessedChar = Validator
-							.getStringMatchingRegex(scnr, userName + ", please enter a letter: ", "[A-za-z]{1}")
-							.charAt(0);
-					guessedChar = Character.toUpperCase(guessedChar);
-					if (guessedLetters.contains(guessedChar)) {
-						System.out.println("You have already guessed this. Please enter another letter: ");
-					} else {
-						done = true;
-					}
-				}
-				
-				guessedChar = Character.toUpperCase(guessedChar);
-				if (Hangman.stringHasChar(hiddenWord, guessedChar)) {
-					partialWord = (Hangman.updatePartialWord(guessedChar, partialWord, hiddenWord));
-					HangmanVisuals.update(guessedLetters, partialWord);
-					if (partialWord.equals(hiddenWord)) {
-						end = true;
-						System.out.println("Hurray, you made it!");
-
-						LeaderBoard.addVictory(userName, 1);
-						break;
-					}
-					end = false;
-				} else {
-					System.out.println("Oops, this word doesn't contain this letter...");
-					guessedLetters.add(guessedChar);
-					HangmanVisuals.update(guessedLetters, partialWord);
-					missedCounter++;
-					if (missedCounter == 9) {
-						end = true;
-						System.out.println("Dang man, now we gotta eat cake!");
-						System.out.println("Here was the correct word: " + hiddenWord);
-						LeaderBoard.addLoss();
-
-					}
-				}
-
-			}
-			System.out.println("Would you like to continue? (y/n) ");
-			userReply = scnr.nextLine().charAt(0);
-		} while (userReply == 'y');
-		LeaderBoard.saveLeaderBoard();
-		System.out.println("Would you like to see the leader board? (y/n): ");
-		userReply = scnr.nextLine().charAt(0);
-		if (userReply == 'y') {
-			LeaderBoard.readLeaderBoard();
-			System.out.println(LeaderBoard.percentageWins() + "%");
-		}
-		System.out.println("Please come come again!");
-	}
-
-=======
 package hangman;
 
 import java.util.ArrayList;
@@ -162,10 +32,13 @@ public class MainHangman {
 			ArrayList<Character> correctLetters = new ArrayList<>();
 			int missedCounter = 0;
 			HangmanVisuals.loadHangMen();
+			
+			// Getting text file of list of possible words
 			ArrayList<String> wordBank = FileHelper.readFromFile();
 			
 			int minLength = 0, maxLength = 0;
 			
+			// Allows user to pick from multiple files of possible words
 			switch(HangmanVisuals.getDiffLevel()) {
 			case EASY:
 				maxLength = 5;
@@ -195,6 +68,7 @@ public class MainHangman {
 			while (!end) {
 				boolean done = false;
 				while (!done) {
+					// Only allows input to be a single letter
 					guessedChar = Validator
 							.getStringMatchingRegex(scnr, userName + ", please enter a letter: ", "[A-za-z]{1}")
 							.charAt(0);
@@ -210,6 +84,7 @@ public class MainHangman {
 					}
 				}
 				
+				// Reveals every place that letter is found in the word
 				guessedChar = Character.toUpperCase(guessedChar);
 				if (Hangman.stringHasChar(hiddenWord, guessedChar)) {
 					partialWord = (Hangman.updatePartialWord(guessedChar, partialWord, hiddenWord));
@@ -231,7 +106,10 @@ public class MainHangman {
 					System.out.println("Oops, this word doesn't contain this letter...");
 					guessedLetters.add(guessedChar);
 					HangmanVisuals.update(guessedLetters, partialWord);
+					
+					// Counter of misses that increments 
 					missedCounter++;
+					// User cannot miss more 9 times
 					if (missedCounter == 9) {
 						end = true;
 						System.out.println("Dang man, now we gotta eat cake!");
@@ -253,5 +131,4 @@ public class MainHangman {
 		scnr.close();
 	}
 
->>>>>>> 8fe6cad03923863b65b157de1626164d76cb54d5
 }
